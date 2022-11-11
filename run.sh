36cc39ba278d32212ba1f22dcbf16ef160766cee
Lucas Zeng <lucasqzeng@gmail.com>
2022-11-11 15:11:27 +1100

    chanign script

--- run.sh
+++ run.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 echo "script started"
-docker run -dp 8080:8080 birdopedia
+# docker run -dp 8080:8080 birdopedia
+# go test -v ./...
+
+# run tests
 go test -v ./...
+
+# kubernetes deployment
+kubectl apply -f kubernetes-deployment.yaml
 echo "script finished"
